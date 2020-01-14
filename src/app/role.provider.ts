f92083d9a2f8057e34005da9389fcb8afe209b9a
Patrizio <patrizio.pezzilli@nexi.it>
2020-01-14 14:21:21 +0100

    fix format

--- src/app/role.provider.ts
+++ src/app/role.provider.ts
@@ -1,28 +1,28 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
-import { NbAuthService, NbAuthJWTToken } from '@nebular/auth';
-import { NbRoleProvider } from '@nebular/security';
-import { User } from './Models/User';
-import { map } from 'rxjs/operators';
+import {NbAuthJWTToken, NbAuthService} from '@nebular/auth';
+import {NbRoleProvider} from '@nebular/security';
+import {User} from 'app/Models/User';
+import {map} from 'rxjs/operators';
 
 
 @Injectable()
 export class AppRoleProvider implements NbRoleProvider {
 
   constructor(private authService: NbAuthService) {
-    }
-  
-    getRole(): Observable<string | string[]> {
-      return this.authService.onTokenChange()
-        .pipe(
-          map((token: NbAuthJWTToken) => {
-          let user : User  =   token.getPayload();
+  }
+
+  getRole(): Observable<string | string[]> {
+    return this.authService.onTokenChange()
+      .pipe(
+        map((token: NbAuthJWTToken) => {
+          let user: User = token.getPayload();
           let defaultRole: Array<string> = ['guest'];
-		      let roles: string[];
-		      roles = token.isValid() ? user.roles : defaultRole;
+          let roles: string[];
+          roles = token.isValid() ? user.roles : defaultRole;
           return roles;
-          }),
-        );
-    }
+        }),
+      );
+  }
 }
