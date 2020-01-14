13f04438883a40ef464ef66b426911b6804be824
Patrizio <patrizio.pezzilli@nexi.it>
2020-01-14 12:42:45 +0100

    first admin console dep

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nebular UI customized to work with Spring Boot Application
+
+* Supports Login Logout with the help of JWT Tokens. Sprinb Boot Backend for this project is available in my github repository.
---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,44 @@
+import {NbMenuItem} from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+
+  {
+    title: 'Dashboard',
+    icon: 'nb-home',
+    link: '/pages/tasks',
+    data: {
+      permission: 'view',
+      resource: 'admin',
+    },
+  },
+  {
+    title: 'Menu',
+    icon: 'nb-home',
+    link: '/pages/tasks',
+    data: {
+      permission: 'view',
+      resource: 'admin',
+    },
+  },
+  {
+    title: 'Menu del Giorno',
+    icon: 'nb-home',
+    link: '/pages/tasks',
+    data: {
+      permission: 'view',
+      resource: 'admin',
+    },
+    children: [
+      {
+        title: 'Inserisci',
+        link: '/pages/extra-components/calendar',
+      },
+      {
+        title: 'Lista sottoscritti',
+        link: '/pages/extra-components/calendar',
+      }
+  },
+]
+}
+]
+;
