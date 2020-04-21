3affb10f531e160a21b452514f1bd123147900f7
Patrizio Pezzilli <patriziopezzilli@gmail.com>
2020-04-21 10:25:15 +0200

    fix scanner

--- src/app/pages/extra-components/components/components.component.ts
+++ src/app/pages/extra-components/components/components.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject} from "rxjs";
+import {BarcodeFormat} from '@zxing/library';
 
 @Component({
   selector: 'components',
@@ -7,12 +8,56 @@ import {HttpClient} from '@angular/common/http';
 })
 export class ComponentsComponent {
 
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+
+  formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE,
+  ];
+
+  hasDevices: boolean;
+  hasPermission: boolean;
+
   result: string;
 
-  constructor(private http: HttpClient) {
+  torchEnabled = false;
+  torchAvailable$ = new BehaviorSubject<boolean>(false);
+  tryHarder = false;
+
+  clearResult(): void {
+    this.result = null;
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
   }
 
   onCodeResult(resultString: string) {
     this.result = resultString;
+  }
+
+  onDeviceSelectChange(selected: string) {
+    const device = this.availableDevices.find(x => x.deviceId === selected);
+    this.currentDevice = device || null;
+  }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
+
+  onTorchCompatible(isCompatible: boolean): void {
+    this.torchAvailable$.next(isCompatible || false);
+  }
+
+  toggleTorch(): void {
+    this.torchEnabled = !this.torchEnabled;
+  }
+
+  toggleTryHarder(): void {
+    this.tryHarder = !this.tryHarder;
   }
 }
